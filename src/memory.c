df2b534b7a5a66b0c27089a387ed7b8bd6580570
stuart <stuartdonaldmiller@gmail.com>
2022-10-16 07:55:37 +0200

    started implementing dynamic array

---
+++ src/memory.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void *pointer, size_t old_size, size_t new_size){
+  if (new_size == 0){
+    free(pointer);
+    return NULL;
+  }
+
+  void *allocated = realloc(pointer, new_size);
+  if (allocated == NULL){
+     exit(1);
+  }
+  return allocated;
+}

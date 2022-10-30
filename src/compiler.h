7df139a52266b295d4536cb57d527a8892d31a68
stuart <stuartdonaldmiller@gmail.com>
2022-10-30 06:48:13 +0100

    add basic stuff for scanner and compiler

---
+++ src/compiler.h
@@ -0,0 +1,6 @@
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+void comiler(const char *source);
+
+#endif
---
+++ src/scanner.h
@@ -0,0 +1,6 @@
+#ifndef scanner_clox_h
+#define scanner_clox_h
+
+void initScanner(const char *source);
+
+#endif

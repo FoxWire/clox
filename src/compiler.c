4f8d880f8af723ea23aab254b4d12c6ffb604c69
stuart <stuartdonaldmiller@gmail.com>
2022-12-11 15:37:42 +0100

    add util functions for error handling to compiler

--- src/compiler.c
+++ src/compiler.c
@@ -1,23 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source){
+typedef struct {
+  Token current;
+  Token previous;
+  bool had_error;
+  bool panic_mode;
+} Parser;
+
+Parser parser;
+
+static void error_at(Token *token, const char *message){
+
+  if (parser.panic_mode){
+    return;
+  }
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == EOF){
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR){
+    // Nothing
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.had_error = true;
+}
+
+static void error(const char *message){
+  error_at(&parser.previous, message);
+}
+
+static void error_at_current(const char *message){
+  error_at(&parser.current, message);
+}
+
+static void advance(){
+  parser.previous = parser.current;
+  while (true){
+    parser.current = scan_token();
+    if (parser.current.type != TOKEN_ERROR){
+      break;
+    }
+    error_at_current(parser.current.start);
+  }
+}
+
+static void consume(TokenType token_type, const char *message){
+  if (parser.current.type == token_type){
+    advance();
+    return;
+  }
+  error_at_current(message);
+}
+
+
+
+
+
+bool compile(const char *source, Chunk *chunk){
   init_scanner(source);
 
-  int line = -1;
-  Token token = scan_token();
-  if (token.line != line){
-    printf("%4d", token.line);
-    line = token.line;
-  } else {
-    printf("   |");
-  }
+  parser.had_error = false;
+  parser.panic_mode = false;
 
-  printf("%2d '%.*s'\n", token.type, token.length, token.start);
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expect end of file expression");
 
-  if (token.type == TOKEN_EOF){
-  }
+  return !parser.had_error;
 }
